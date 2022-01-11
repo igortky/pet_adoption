761e074096af5b3bea1789100b32ff20e7b87e3c
igortky <igortky@gmail.com>
2022-01-11 17:09:18 -0300

    Destroy
    destroy pet implementation

--- app/controllers/pet_controller.rb
+++ app/controllers/pet_controller.rb
@@ -1,7 +1,12 @@
 class PetController < ApplicationController
   def create
     #(name: params[:name])
-    Pet.create(name: params[:name], checkin: params[:checkin], checkout: params[:checkout], birthdate: params[:birthdate])
+    Pet.create(name: params[:name], checkin: params[:checkin],
+               checkout: params[:checkout],
+               birthdate: params[:birthdate])
   end
 
+  def destroy
+    Pet.destroy(params[:id])
+  end
 end
--- spec/requests/pet_request_spec.rb
+++ spec/requests/pet_request_spec.rb
@@ -1,16 +1,26 @@
 require 'rails_helper'
 
   describe PetController do
-
     describe '#create' do
       it 'create a new pet' do
         post '/pet', params: { name: 'agnes', checkin: Date.today,
                                checkout: Date.tomorrow,
                                birthdate: Date.today - 3.years}
 
-
         expect(Pet.count).to be 1
+      end
+    end
 
+    describe '#destroy' do
+      before do
+        @pet = Pet.create( name: 'agnes', checkin: Date.today,
+                           checkout: Date.tomorrow,
+                           birthdate: Date.today - 3.years)
+      end
+
+      it 'destroy a pet' do
+        delete "/pet/#{@pet.id}"
+        expect(Pet.count).to be 0
       end
     end
   end
